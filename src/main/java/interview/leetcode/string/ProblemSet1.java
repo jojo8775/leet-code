43b7b0ca1181d76e6d0bba5b4d71790e861b1e6d
jojo <chiranjeeb.nandy@yahoo.com>
2016-01-31 14:54:30 -0600

    Added code

--- src/main/java/interview/leetcode/string/ProblemSet1.java
+++ src/main/java/interview/leetcode/string/ProblemSet1.java
@@ -497,4 +497,35 @@ public class ProblemSet1
 
 		return maxSoFar;
 	}
+
+	/**
+	 * Say you have an array for which the ith element is the price of a given
+	 * stock on day i.
+	 * 
+	 * If you were only permitted to complete at most one transaction (ie, buy
+	 * one and sell one share of the stock), design an algorithm to find the
+	 * maximum profit.
+	 * 
+	 * Subscribe to see which companies asked this question
+	 * 
+	 * Show Tags Show Similar Problems
+	 */
+	public int maxProfit(int[] prices)
+	{
+		if (prices.length == 0)
+		{
+			return 0;
+		}
+
+		int sell = 0;
+		int buy = Integer.MAX_VALUE;
+
+		for (int i = 0; i < prices.length; i++)
+		{
+			buy = Math.min(buy, prices[i]);
+			sell = Math.max(sell, prices[i] - buy);
+		}
+
+		return sell;
+	}
 }
