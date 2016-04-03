13807e2b2bebaf9eccd707b866e7be54dbcd7532
jojo <chiranjeeb.nandy@yahoo.com>
2016-04-03 01:19:48 -0500

    Added histogram area

--- src/main/java/interview/leetcode/tushar/assorted/MaxRechtriangleInHistoram.java
+++ src/main/java/interview/leetcode/tushar/assorted/MaxRechtriangleInHistoram.java
@@ -4,18 +4,23 @@ import java.util.Stack;
 
 public class MaxRechtriangleInHistoram
 {
-	private int findmaxArea(int[] arr){
+	public static void main(String[] args){
+		int[] arr = {1,2,2,3, 0, 4, 4};
+		System.out.println(findMaxArea(arr));
+	}
+	
+	private static int findMaxArea(int[] arr){
 		
 		Stack<Integer> stack = new Stack<Integer>();
 		int maxArea = arr[0], currentArea = 0;
 		int maxBegIndex = 0, maxEndIndex = 0;
 		
 		for(int i=0 ; i < arr.length; i++){
-			if(arr[i] > 0 || stack.isEmpty() || arr[stack.peek()] <= arr[i]){
+			if(stack.isEmpty() || arr[stack.peek()] <= arr[i]){
 				stack.push(i);
 			}
 			else{
-				while(arr[i] > 0 || !stack.isEmpty() || arr[stack.peek()] <= arr[i]){
+				while(!stack.isEmpty() && arr[stack.peek()] > arr[i]){
 					int storedIndex = stack.pop();
 					currentArea = arr[storedIndex] * (i - storedIndex);
 					
@@ -30,14 +35,11 @@ public class MaxRechtriangleInHistoram
 			}
 		}
 		
-		int refIndex = stack.isEmpty() ? 0 : stack.peek();
-		if(refIndex == 0){
-			return maxArea;
-		}
+		int refIndex = arr.length;
 		
 		while(!stack.isEmpty()){
 			int storedIndex = stack.pop();
-			currentArea = storedIndex * refIndex;
+			currentArea = arr[storedIndex] * (refIndex - storedIndex);
 			if(currentArea > maxArea){
 				maxArea = currentArea;
 				maxBegIndex = storedIndex;
