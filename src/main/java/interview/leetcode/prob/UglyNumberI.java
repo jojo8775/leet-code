dafc2d69a2819ec69c953220c8289593e97e78fd
jojo <chiranjeeb.nandy@yahoo.com>
2016-07-04 11:41:41 -0500

    Leet code prob

---
+++ src/main/java/interview/leetcode/prob/UglyNumberI.java
@@ -0,0 +1,53 @@
+package interview.leetcode.prob;
+
+/**
+ * Write a program to check whether a given number is an ugly number.
+ * 
+ * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+ * For example, 6, 8 are ugly while 14 is not ugly since it includes another
+ * prime factor 7.
+ * 
+ * Note that 1 is typically treated as an ugly number.
+ * 
+ * @author jojo
+ *
+ */
+public class UglyNumberI
+{
+	public boolean isUgly(int num)
+	{
+		if (num < 0)
+		{
+			return false;
+		}
+		if (num == 1)
+		{
+			return true;
+		}
+
+		if (num % 2 == 0)
+		{
+			num = num / 2;
+			return isUgly(num);
+		}
+
+		if (num % 3 == 0)
+		{
+			num = num / 3;
+			return isUgly(num);
+		}
+
+		if (num % 5 == 0)
+		{
+			num = num / 5;
+			return isUgly(num);
+		}
+
+		return false;
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println(new UglyNumberI().isUgly(Integer.MIN_VALUE));
+	}
+}
