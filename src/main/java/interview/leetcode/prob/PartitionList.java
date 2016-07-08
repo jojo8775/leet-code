65b7880fd0efb7cdab54094bfcbf8af0e484295f
jojo <chiranjeeb.nandy@yahoo.com>
2016-07-08 15:48:31 -0500

    leet code prob

--- src/main/java/interview/leetcode/prob/PartitionList.java
+++ src/main/java/interview/leetcode/prob/PartitionList.java
@@ -9,7 +9,7 @@ package interview.leetcode.prob;
  * 
  * For example, Given 1->4->3->2->5->2 and x = 3, return 1->2->2->4->3->5.
  * 
- * @author jojo
+ * @author j61ojo
  *
  */
 public class PartitionList {
