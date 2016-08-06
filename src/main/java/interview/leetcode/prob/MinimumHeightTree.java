45d4dc99494ba1eeccf3cb97f840bf891a891cca
jojo <chiranjeeb.nandy@yahoo.com>
2016-08-05 20:16:14 -0500

    Leet code prob

---
+++ src/main/java/interview/leetcode/prob/MinimumHeightTree.java
@@ -0,0 +1,61 @@
+package interview.leetcode.prob;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class MinimumHeightTree {
+	public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+		if (n == 1)
+			return Collections.singletonList(0);
+
+		List<Set<Integer>> adj = new ArrayList<Set<Integer>>(n);
+		for (int i = 0; i < n; ++i)
+			adj.add(new HashSet<Integer>());
+		for (int[] edge : edges) {
+			adj.get(edge[0]).add(edge[1]);
+			adj.get(edge[1]).add(edge[0]);
+		}
+
+		List<Integer> leaves = new ArrayList<Integer>();
+		for (int i = 0; i < n; ++i)
+			if (adj.get(i).size() == 1) {
+				leaves.add(i);
+			}
+
+		while (n > 2) {
+			n -= leaves.size();
+			List<Integer> newLeaves = new ArrayList<Integer>();
+			for (int i : leaves) {
+				int j = adj.get(i).iterator().next();
+				adj.get(j).remove(i);
+				if (adj.get(j).size() == 1){
+					newLeaves.add(j);
+				}
+			}
+			leaves = newLeaves;
+		}
+		return leaves;
+	}
+	
+	public static void main(String[] args){
+		int[][] edges = new int[5][];
+		edges[0] = createArr(0,3);
+		edges[1] = createArr(1,3);
+		edges[2] = createArr(2,3);
+		edges[3] = createArr(4,3);
+		edges[4] = createArr(5,4);
+		
+		List<Integer> result = new MinimumHeightTree().findMinHeightTrees(6, edges);
+		
+		for(int i: result){
+			System.out.println(i);
+		}
+	}
+	
+	private static int[] createArr(int ... i){
+		return i;
+	}
+}
---
+++ src/main/java/interview/leetcode/prob/WiggleSlotII.java
@@ -0,0 +1,84 @@
+package interview.leetcode.prob;
+
+/**
+ * Given an unsorted array nums, reorder it such that nums[0] < nums[1] > nums[2] < nums[3]....
+
+Example:
+(1) Given nums = [1, 5, 1, 1, 6, 4], one possible answer is [1, 4, 1, 5, 1, 6]. 
+(2) Given nums = [1, 3, 2, 2, 3, 1], one possible answer is [2, 3, 1, 3, 1, 2].
+
+Note:
+You may assume all input has valid answer.
+
+Follow Up:
+Can you do it in O(n) time and/or in-place with O(1) extra space?
+ * @author jojo
+ *
+ */
+public class WiggleSlotII {
+	private int length;
+
+	public void wiggleSort(int[] nums) {
+		// quick select O(n) + index mapping O(1)
+		length = nums.length;
+		int middle = quickselect(nums, 0, nums.length - 1, (nums.length - 1) / 2);
+		int curr = 0;
+		int left = 0;
+		int right = nums.length - 1;
+		while (curr <= right) {
+			if (nums[mapping(curr)] > middle) {
+				swap(nums, mapping(curr), mapping(left));
+				curr++;
+				left++;
+			} else if (nums[mapping(curr)] < middle) {
+				swap(nums, mapping(curr), mapping(right));
+				right--;
+			} else {
+				curr++;
+			}
+		}
+	}
+
+	private void swap(int[] nums, int i, int j) {
+		int temp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = temp;
+	}
+
+	private int mapping(int i) {
+		return (i * 2 + 1) % (length | 1);
+	}
+
+	private int quickselect(int[] nums, int left, int right, int k) {
+		if (left >= right) {
+			return nums[left];
+		}
+		int position = partition(nums, left, right);
+		if (position == k) {
+			return nums[position];
+		} else if (position < k) {
+			return quickselect(nums, position + 1, right, k);
+		} else {
+			return quickselect(nums, left, position - 1, k);
+		}
+	}
+
+	private int partition(int[] nums, int left, int right) {
+		if (left >= right) {
+			return left;
+		}
+		int value = nums[left];
+		while (left < right) {
+			while (left < right && nums[right] >= value) {
+				right--;
+			}
+			nums[left] = nums[right];
+			while (left < right && nums[left] < value) {
+				left++;
+			}
+			nums[right] = nums[left];
+		}
+		nums[left] = value;
+		return left;
+	}
+}
