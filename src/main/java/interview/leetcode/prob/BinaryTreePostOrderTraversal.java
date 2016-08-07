d6eaa209f4698e2e9a3bd8876b1b62f4d8f29c92
jojo <chiranjeeb.nandy@yahoo.com>
2016-08-07 17:38:47 -0500

    Added code

---
+++ src/main/java/interview/leetcode/prob/BinaryTreePostOrderTraversal.java
@@ -0,0 +1,71 @@
+package interview.leetcode.prob;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+
+/*-
+ * Given a binary tree, return the postorder traversal of its nodes' values.
+
+For example:
+Given binary tree {1,#,2,3},
+   1
+    \
+     2
+    /
+   3
+return [3,2,1].
+
+Note: Recursive solution is trivial, could you do it iteratively?
+
+
+ */
+public class BinaryTreePostOrderTraversal {
+	public List<Integer> postorderTraversal(TreeNode root) {
+		List<Integer> result = new ArrayList<Integer>();
+		if (root == null) {
+			return result;
+		}
+
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		stack.push(root);
+		Set<TreeNode> visited = new HashSet<TreeNode>();
+		visited.add(null);
+
+		while (!stack.isEmpty()) {
+			TreeNode node = stack.peek();
+			while (node != null) {
+				if (!visited.contains(node.right)) {
+					stack.push(node.right);
+				}
+
+				if (!visited.contains(node.left)) {
+					stack.push(node.left);
+				}
+
+				node = node.left;
+			}
+
+			node = stack.peek();
+			if (visited.contains(node.left) && visited.contains(node.right)) {
+				visited.add(node);
+				result.add(node.val);
+				stack.pop();
+			}
+		}
+
+		return result;
+	}
+
+	private static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+}
