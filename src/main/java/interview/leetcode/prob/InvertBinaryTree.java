7de9b0210e24d9f0dd499b0be767298f12092695
jojo <chiranjeeb.nandy@yahoo.com>
2016-08-18 00:57:24 -0500

    leet code prob

---
+++ src/main/java/interview/leetcode/prob/InvertBinaryTree.java
@@ -0,0 +1,46 @@
+package interview.leetcode.prob;
+
+/**
+ * Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+ * @author jojo
+ *
+ */
+public class InvertBinaryTree {
+	public TreeNode invertTree(TreeNode root) {
+		if (root == null) {
+			return null;
+		}
+
+		TreeNode temp = root.left;
+		root.left = root.right;
+		root.right = temp;
+
+		root.left = invertTree(root.left);
+		root.right = invertTree(root.right);
+
+		return root;
+	}
+
+	private static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+}
