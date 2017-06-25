cf6b0a30d5446cc65320db55505ffa295c85c482
jojo <chiranjeeb.nandy@yahoo.com>
2017-06-25 11:32:12 -0500

    Problem practice

---
+++ src/main/java/interview/leetcode/prob/MaxProductOfThreeNumbers.java
@@ -0,0 +1,35 @@
+package interview.leetcode.prob;
+
+/**
+ * Given an integer array, find three numbers whose product is maximum and
+ * output the maximum product.
+ * 
+ * Example 1: Input: [1,2,3] Output: 6 Example 2: Input: [1,2,3,4] Output: 24
+ * Note: The length of the given array will be in range [3,104] and all elements
+ * are in the range [-1000, 1000]. Multiplication of any three numbers in the
+ * input won't exceed the range of 32-bit signed integer.
+ * 
+ * @author jojo Jun 25, 201711:31:46 AM
+ */
+public class MaxProductOfThreeNumbers {
+    public int maximumProduct(int[] nums) {
+        Integer num1 = null, num2 = null, num3 = null;
+        
+        for(int i=0; i<nums.length; i++){
+            if(num1 == null || num1 < nums[i]){
+                num3 = num2;
+                num2 = num1;
+                num1 = nums[i];
+            }
+            else if(num2 == null || num2 < nums[i]){
+                num3 = num2;
+                num2 = nums[i];
+            }
+            else if(num3 == null || num3 < nums[i]){
+                num3 = nums[i];
+            }
+        }
+        
+        return num1 * num2 * num3;
+    }
+}
