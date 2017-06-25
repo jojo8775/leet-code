92683db825b3955ad68c0fbeb4e51c34f79d6ef8
jojo <chiranjeeb.nandy@yahoo.com>
2017-06-25 11:53:45 -0500

    Problem practice

--- src/main/java/interview/leetcode/prob/MaxProductOfThreeNumbers.java
+++ src/main/java/interview/leetcode/prob/MaxProductOfThreeNumbers.java
@@ -15,21 +15,33 @@ public class MaxProductOfThreeNumbers {
     public int maximumProduct(int[] nums) {
         Integer num1 = null, num2 = null, num3 = null;
         
+        int neg1 = 0, neg2 = 0;
+        
         for(int i=0; i<nums.length; i++){
-            if(num1 == null || num1 < nums[i]){
+            if(num1 == null || num1 <= nums[i]){
                 num3 = num2;
                 num2 = num1;
                 num1 = nums[i];
             }
-            else if(num2 == null || num2 < nums[i]){
+            else if(num2 == null || num2 <= nums[i]){
                 num3 = num2;
                 num2 = nums[i];
             }
-            else if(num3 == null || num3 < nums[i]){
+            else if(num3 == null || num3 <= nums[i]){
                 num3 = nums[i];
+            }
+            
+            if(nums[i] < 0){
+                if(neg1 >= nums[i]){
+                    neg2 = neg1;
+                    neg1 = nums[i];
+                }
+                else if(neg2 >= nums[i]){
+                    neg2 = nums[i];
+                }
             }
         }
         
-        return num1 * num2 * num3;
+        return Math.max(num1 * num2 * num3, num1 * neg1 * neg2);
     }
 }
