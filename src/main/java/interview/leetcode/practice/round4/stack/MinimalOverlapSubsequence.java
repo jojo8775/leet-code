b4c6de692cc6dc3b8c0177e01f9959aeec543da8
Jojo <chiranjeeb.nandy@yahoo.com>
2018-02-03 16:56:49 -0800

    Added problem

--- src/main/java/interview/leetcode/practice/round4/stack/MinimalOverlapSubsequence.java
+++ src/main/java/interview/leetcode/practice/round4/stack/MinimalOverlapSubsequence.java
@@ -1,7 +1,9 @@
 package interview.leetcode.practice.round4.stack;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
@@ -9,6 +11,9 @@ public class MinimalOverlapSubsequence {
     public int[] findMinimalOverlapSubsequence(String str) {
         // using this to find the index of each character
         Map<Character, Integer> charIndex = new HashMap<>();
+        charIndex.values();
+        Arrays.asList(0);
+        
         
         // using this to track repeated character sequence
         int[] arr = new int[str.length()];
@@ -16,7 +21,9 @@ public class MinimalOverlapSubsequence {
         // marking termination
         arr[0] = -1;
         charIndex.put(str.charAt(0), 0);
-
+        
+        List<String> l = new ArrayList<>();
+        
         // populating possible sequence break points.
         for (int i = 1; i < arr.length; i++) {
             char ch = str.charAt(i);
