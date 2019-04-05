5db3579b3eea8f041596b347304b52f948ac223b
Jojo Nandy <chiranjeeb.nandy@yahoo.com>
2019-04-04 21:17:08 -0700

    added solution fix

--- src/main/java/interview/leetcode/prob/MaxPointOnALine.java
+++ src/main/java/interview/leetcode/prob/MaxPointOnALine.java
@@ -3,6 +3,8 @@ package interview.leetcode.prob;
 import java.util.HashMap;
 import java.util.Map;
 
+import interview.leetcode.practice.MaxPointsOnALine.Point;
+
 /**
  * Given n points on a 2D plane, find the maximum number of points that lie on
  * the same straight line.
@@ -11,9 +13,53 @@ import java.util.Map;
  *
  */
 public class MaxPointOnALine {
+	public int maxPoints(Point[] points) {
+		if (points.length < 3) {
+			return points.length;
+		}
+
+		int maxCount = 1; // begin with one point.
+		for (int i = 0; i < points.length - 1; i++) {
+			Map<Double, Integer> slopeMap = new HashMap<>();
+			// if there is only one point, it can contribute to horizontal.
+			int horizontals = 1; 
+			// this is current count and since we start with one node.
+			int currentCount = 1; 
+			// since there is only one node at start, no duplicates
+			int duplicates = 0; 
+
+			for (int j = i + 1; j < points.length; j++) {
+				// if both the points are same
+				if ((points[i].x == points[j].x) && (points[i].y == points[j].y)) {
+					duplicates++;
+				}
+				// if they lie on the same x-axis
+				else if (points[i].y == points[j].y) {
+					horizontals++;
+					// keeping track of current max count
+					currentCount = Math.max(horizontals, currentCount);
+				} else {
+					// calculating slope
+					double slope = 1.0 * (points[i].x - points[j].x) / (points[i].y - points[j].y) + 0.0;
+					// if there is no entry then adding 2 else incrementing by 1
+					slopeMap.put(slope, slopeMap.getOrDefault(slope, 1) + 1);
+					
+					// keeping track of the current max count
+					currentCount = Math.max(slopeMap.get(slope), currentCount);
+				}
+			}
+
+			// considering max of points on the same x-axis VS with in a same slope.
+			maxCount = Math.max(maxCount, currentCount + duplicates);
+		}
+		
+		return maxCount;
+	}
+	
+	
 	// this can be computed by considering the slot, duplicate and vertical
 	// points.
-	public int maxPoints(Point[] points) {
+	public int maxPoints_old(Point[] points) {
 		if (points.length == 0) {
 			return 0;
 		}
