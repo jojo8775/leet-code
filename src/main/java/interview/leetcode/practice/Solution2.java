e0d0bea83d78c658d70616d63ae68c140d12e8e2
Jojo Nandy <chiranjeeb.nandy@yahoo.com>
2019-07-21 09:47:57 -0700

    Added solution

--- src/main/java/interview/leetcode/practice/Solution2.java
+++ src/main/java/interview/leetcode/practice/Solution2.java
@@ -1,16 +1,199 @@
 package interview.leetcode.practice;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 public class Solution2 {
 	public static void main(String[] args) {
-		int result = new Solution2()
-				.earliestAcq(
-						new int[][] {{20190101,0,1},{20190104,3,4},{20190107,2,3},{20190211,1,5},{20190224,2,4},{20190301,0,3},{20190312,1,2},{20190322,4,5}},
-						6);
-		System.out.println(result);
+		List<List<Integer>> result = new Solution2()
+				.palindromePairs(new String[] { "abcd", "dcba", "lls", "s", "sssll" });
+
+		result.forEach(x -> {
+			x.forEach(xx -> System.out.print(xx + ", "));
+			System.out.println();
+		});
+	}
+
+	public class NestedIterator implements Iterator<Integer> {
+
+	    private Deque<NestedInteger> queue = new LinkedList<>();
+	    private NestedInteger cur;
+	    
+	    public NestedIterator(List<NestedInteger> nestedList) {
+	        for(NestedInteger n : nestedList){
+	            queue.offerLast(n);
+	        }
+	        
+	        cur = getNext();
+	    }
+	    
+	    private NestedInteger getNext(){
+	        NestedInteger next = null;
+	        
+	        while(next == null){
+	            
+	            if(queue.isEmpty()){
+	                break;
+	            }
+	            
+	            NestedInteger top = queue.poll();
+	            if(top.isInteger()){
+	                next = top;
+	            }
+	            else{
+	                for(NestedInteger n : top.getList()){
+	                    queue.offerFirst(n);
+	                }
+	            }
+	        }
+	        
+	        return next;
+	    }
+
+	    @Override
+	    public Integer next() {
+	        NestedInteger temp = cur;
+	        cur = getNext();
+	        return temp.getInteger();
+	    }
+
+	    @Override
+	    public boolean hasNext() {
+	        return cur != null;
+	    }
+	}
+
+	
+	public interface NestedInteger {
+		boolean isInteger();
+
+		Integer getInteger();
+
+		List<NestedInteger> getList();
+	}
+
+	public List<List<Integer>> palindromePairs(String[] words) {
+		TrieNode root = new TrieNode();
+
+		for (int i = 0; i < words.length; i++) {
+			insertWord(words[i], i, root);
+		}
+
+		List<List<Integer>> result = new ArrayList<>();
+
+		for (int i = 0; i < words.length; i++) {
+			search(words[i], i, root, result);
+		}
+
+		return result;
+	}
+
+	private void insertWord(String word, int idx, TrieNode node) {
+		for (int i = word.length() - 1; i >= 0; i--) {
+			char ch = word.charAt(i);
+			if (node.next[ch - 'a'] == null) {
+				node.next[ch - 'a'] = new TrieNode();
+			}
+
+			if (isPalindrom(word, 0, i)) {
+				node.pIndex.add(idx);
+			}
+
+			node = node.next[ch - 'a'];
+		}
+
+		node.index = idx;
+		node.pIndex.add(idx);
+	}
+
+	private void search(String word, int idx, TrieNode node, List<List<Integer>> result) {
+		for (int i = 0; i < word.length(); i++) {
+			char ch = word.charAt(i);
+			if (node.index != -1 && node.index != idx && isPalindrom(word, i, word.length() - 1)) {
+				result.add(Arrays.asList(idx, node.index));
+			}
+
+			if (node.next[ch - 'a'] == null) {
+				return;
+			}
+
+			node = node.next[ch - 'a'];
+		}
+
+		for (int k : node.pIndex) {
+			if (k == idx) {
+				continue;
+			}
+
+			result.add(Arrays.asList(k, idx));
+		}
+	}
+
+	private boolean isPalindrom(String word, int beg, int end) {
+		while (beg < end) {
+			if (word.charAt(beg++) != word.charAt(end--)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private static class TrieNode {
+		int index = -1;
+		TrieNode[] next = new TrieNode[26];
+		List<Integer> pIndex = new ArrayList<>();
+	}
+
+	public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+		List<Set<Integer>> list = new ArrayList<>();
+
+		if (edges.length == 0) {
+			return Arrays.asList(0);
+		}
+
+		for (int i = 0; i <= n; i++) {
+			list.add(new HashSet<Integer>());
+		}
+
+		for (int[] edge : edges) {
+			list.get(edge[0]).add(edge[1]);
+			list.get(edge[1]).add(edge[0]);
+		}
+
+		List<Integer> leaf = new ArrayList<>();
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).size() == 1) {
+				leaf.add(i);
+			}
+		}
+
+		while (n > 2) {
+			List<Integer> nextLeafs = new ArrayList<>();
+			for (int x : leaf) {
+				int j = list.get(x).iterator().next();
+				list.get(j).remove(x);
+				n--;
+
+				if (list.get(j).size() == 1) {
+					nextLeafs.add(j);
+				}
+			}
+			;
+
+			leaf = nextLeafs;
+		}
+
+		return leaf;
 	}
 
 	public int earliestAcq(int[][] logs, int N) {
