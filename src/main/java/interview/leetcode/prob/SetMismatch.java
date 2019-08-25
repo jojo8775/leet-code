ce38f8b742f24163b60d507d96f1d56ada56be32
Jojo Nandy <chiranjeeb.nandy@yahoo.com>
2019-08-25 03:45:07 -0700

    added solution

--- src/main/java/interview/leetcode/prob/SetMismatch.java
+++ src/main/java/interview/leetcode/prob/SetMismatch.java
@@ -15,6 +15,33 @@ The given array's numbers won't have any order.
  *
  */
 public class SetMismatch {
+	public int[] findErrorNums_2nd(int[] nums) {
+        int[] result = new int[2];
+        
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] - 1 != i && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] - 1 != i) {
+                result[0] = nums[i];
+                result[1] = i + 1;
+                break;
+            }
+        }
+        
+        return result;
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+	
+	
     public int[] findErrorNums(int[] nums) {
         int[] result = new int[2];
         for(int i=0; i<nums.length; i++){
