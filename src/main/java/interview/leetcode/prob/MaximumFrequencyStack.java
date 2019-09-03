2f4427ab320ffb70496f981007de8043da612a1c
Jojo Nandy <chiranjeeb.nandy@yahoo.com>
2019-09-02 18:02:48 -0700

    added solution

---
+++ src/main/java/interview/leetcode/prob/MaximumFrequencyStack.java
@@ -0,0 +1,80 @@
+package interview.leetcode.prob;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * Implement FreqStack, a class which simulates the operation of a stack-like data structure.
+
+FreqStack has two functions:
+
+push(int x), which pushes an integer x onto the stack.
+pop(), which removes and returns the most frequent element in the stack.
+If there is a tie for most frequent element, the element closest to the top of the stack is removed and returned.
+ 
+
+Example 1:
+
+Input: 
+["FreqStack","push","push","push","push","push","push","pop","pop","pop","pop"],
+[[],[5],[7],[5],[7],[4],[5],[],[],[],[]]
+Output: [null,null,null,null,null,null,null,5,7,5,4]
+Explanation:
+After making six .push operations, the stack is [5,7,5,7,4,5] from bottom to top.  Then:
+
+pop() -> returns 5, as 5 is the most frequent.
+The stack becomes [5,7,5,7,4].
+
+pop() -> returns 7, as 5 and 7 is the most frequent, but 7 is closest to the top.
+The stack becomes [5,7,5,4].
+
+pop() -> returns 5.
+The stack becomes [5,7,4].
+
+pop() -> returns 4.
+The stack becomes [5,7].
+ 
+
+Note:
+
+Calls to FreqStack.push(int x) will be such that 0 <= x <= 10^9.
+It is guaranteed that FreqStack.pop() won't be called if the stack has zero elements.
+The total number of FreqStack.push calls will not exceed 10000 in a single test case.
+The total number of FreqStack.pop calls will not exceed 10000 in a single test case.
+The total number of FreqStack.push and FreqStack.pop calls will not exceed 150000 across all test cases.
+
+ * @author jojo
+ * Sep 2, 2019 6:02:21 PM
+ */
+public class MaximumFrequencyStack {
+	class FreqStack {
+	    Map<Integer, Integer> frequencyMap = new HashMap<>();
+	    Map<Integer, Stack<Integer>> stackMap = new HashMap<>();
+	    int maxFrequency = 0;
+	    
+	    public FreqStack() {
+	        
+	    }
+	    
+	    public void push(int x) {
+	        int frequency = frequencyMap.getOrDefault(x, 0) + 1;
+	        frequencyMap.put(x, frequency);
+	        
+	        maxFrequency = Math.max(maxFrequency,frequency);
+	        
+	        stackMap.computeIfAbsent(frequency, v -> new Stack<Integer>()).push(x);
+	    }
+	    
+	    public int pop() {
+	        int val = stackMap.get(maxFrequency).pop();
+	        frequencyMap.put(val, maxFrequency - 1);
+	        
+	        if(stackMap.get(maxFrequency).isEmpty()){
+	            maxFrequency--;
+	        }
+	        
+	        return val;
+	    }
+	}
+}
