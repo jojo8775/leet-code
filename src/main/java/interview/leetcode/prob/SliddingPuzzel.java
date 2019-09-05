516a2a2cc6b46d19cdb0cae13468f2ee37a2a6d0
Jojo Nandy <chiranjeeb.nandy@yahoo.com>
2019-09-05 01:40:06 -0700

    added solution

---
+++ src/main/java/interview/leetcode/prob/SliddingPuzzel.java
@@ -0,0 +1,102 @@
+package interview.leetcode.prob;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+/**
+ * On a 2x3 board, there are 5 tiles represented by the integers 1 through 5, and an empty square represented by 0.
+
+A move consists of choosing 0 and a 4-directionally adjacent number and swapping it.
+
+The state of the board is solved if and only if the board is [[1,2,3],[4,5,0]].
+
+Given a puzzle board, return the least number of moves required so that the state of the board is solved. If it is impossible for the state of the board to be solved, return -1.
+
+Examples:
+
+Input: board = [[1,2,3],[4,0,5]]
+Output: 1
+Explanation: Swap the 0 and the 5 in one move.
+Input: board = [[1,2,3],[5,4,0]]
+Output: -1
+Explanation: No number of moves will make the board solved.
+Input: board = [[4,1,2],[5,0,3]]
+Output: 5
+Explanation: 5 is the smallest number of moves that solves the board.
+An example path:
+After move 0: [[4,1,2],[5,0,3]]
+After move 1: [[4,1,2],[0,5,3]]
+After move 2: [[0,1,2],[4,5,3]]
+After move 3: [[1,0,2],[4,5,3]]
+After move 4: [[1,2,0],[4,5,3]]
+After move 5: [[1,2,3],[4,5,0]]
+Input: board = [[3,2,4],[1,5,0]]
+Output: 14
+Note:
+
+board will be a 2 x 3 array as described above.
+board[i][j] will be a permutation of [0, 1, 2, 3, 4, 5].
+ * @author jojo
+ * Sep 5, 2019 1:39:43 AM
+ */
+public class SliddingPuzzel {
+	public int slidingPuzzle(int[][] board) {
+        // consider in terms of graph and convert the board into string, to represet it as a node in a graph
+        String target = "123450", start = "";
+    
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                start += board[i][j];
+            }
+        }
+        
+        // write board in two rows 
+        // 
+        // 0 1 2 
+        // 3 4 5
+        // 
+        // from top left possible places to go are 1,3. Simillarly from 1 possible places to go are 0,4,2 and so on.
+        int[][] dirs = {{1,3}, {0,4,2}, {1,5}, {0,4}, {3,1,5}, {2,4}};
+        
+        Queue<String> queue = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
+        
+        queue.offer(start);
+        visited.add(start);
+        
+        int movesCount = 0;
+        
+        while(!queue.isEmpty()){
+            for(int i=queue.size() - 1; i>=0; i--){
+                String top = queue.poll();
+                
+                if(top.equals(target)){
+                    return movesCount;
+                }
+                
+                int zeroIdx = top.indexOf("0");
+                for(int move : dirs[zeroIdx]){
+                    String str = swap(top, zeroIdx, move);
+                    if(visited.add(str)){
+                        queue.offer(str);
+                    }
+                }
+            }
+            
+            movesCount++;
+        }
+        
+        return -1;
+    }
+    
+    private String swap(String str, int i, int j){
+        char[] cArr = str.toCharArray();
+        char temp = cArr[i];
+        cArr[i] = cArr[j];
+        cArr[j] = temp;
+        
+        return String.valueOf(cArr);
+    }
+}
