052c9fb29fa410220936cf881df07b1bef991f7f
Jojo Nandy <chiranjeeb.nandy@yahoo.com>
2019-09-10 00:23:06 -0700

    comilation fix

--- src/main/java/interview/leetcode/prob/EmployeeFreeTime.java
+++ src/main/java/interview/leetcode/prob/EmployeeFreeTime.java
@@ -78,5 +78,10 @@ public class EmployeeFreeTime {
     
     private static class Interval{
     	int start, end;
+    	
+    	public Interval(int start, int end) {
+    		this.start = start;
+    		this.end = end;
+    	}
     }
 }
