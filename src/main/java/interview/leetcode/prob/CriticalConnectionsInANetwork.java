98114103b28349ec64036f570b9036be3612089d
Jojo Nandy <chiranjeeb.nandy@yahoo.com>
2020-05-07 00:41:23 -0700

    Added solution

--- src/main/java/interview/leetcode/prob/CriticalConnectionsInANetwork.java
+++ src/main/java/interview/leetcode/prob/CriticalConnectionsInANetwork.java
@@ -2,41 +2,74 @@ package interview.leetcode.prob;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
+/**
+ * some server unable to reach some other server.
+
+Return all critical connections in the network in any order.
+
+ 
+
+Example 1:
+
+
+
+Input: n = 4, connections = [[0,1],[1,2],[2,0],[1,3]]
+Output: [[1,3]]
+Explanation: [[3,1]] is also accepted.
+ 
+
+Constraints:
+
+1 <= n <= 10^5
+n-1 <= connections.length <= 10^5
+connections[i][0] != connections[i][1]
+There are no repeated connections.
+ * @author jojo
+ * May 7, 2020  12:32:11 AM
+ */
 public class CriticalConnectionsInANetwork {
 	public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        Map<Integer, Set<Integer>> map = new HashMap<>();
+        List<Integer>[] adjacent = new ArrayList[n];
+        for(int i=0; i<n; i++) {
+        	adjacent[i] = new ArrayList<Integer>();
+        }
         
-        for(List<Integer> conn : connections){
-            Set<Integer> val1 = map.get(conn.get(0));
-            if(val1 == null){
-                val1 = new HashSet<Integer>();
-                map.put(conn.get(0), val1);
-            }
-            
-            val1.add(conn.get(1));
-            
-            Set<Integer> val2 = map.get(conn.get(1));
-            if(val2 == null){
-                val2 = new HashSet<Integer>();
-                map.put(conn.get(1), val2);
-            }
-            
-            val2.add(conn.get(0));
+        for(List<Integer> connection : connections) {
+        	adjacent[connection.get(0)].add(connection.get(1));
+        	adjacent[connection.get(1)].add(connection.get(0));
         }
         
         List<List<Integer>> result = new ArrayList<>();
-        for(Map.Entry<Integer, Set<Integer>> entry : map.entrySet()){
-            if(entry.getValue().size() == 1){
-                result.add(Arrays.asList(entry.getKey(), entry.getValue().iterator().next()));
-            }
-        }
+        int[] visited = new int[n];
+        int[] lowRank = new int[n];
+        
+        dfs(adjacent, visited, lowRank, 0, -1, new int[] {0}, result);
         
         return result;
     }
+	
+	private int dfs(List<Integer>[] adjacent, int[] visited, int[] lowRank, int curNode, int parentNode, int[] counter, List<List<Integer>> result) {
+		if(visited[curNode] == 1) {
+			return lowRank[curNode];
+		}
+		
+		visited[curNode] = 1;
+		lowRank[curNode] = counter[0]++;
+		int curVal = lowRank[curNode];
+		
+		for(int nei : adjacent[curNode]) {
+			if(nei != parentNode) {
+				int neiMin = dfs(adjacent, visited, lowRank, nei, curNode, counter, result);
+				if(curVal < neiMin) {
+					result.add(Arrays.asList(curNode, nei));
+				}
+				
+				lowRank[curNode] = Math.min(lowRank[curNode], neiMin);
+			}
+		}
+		
+		return lowRank[curNode];
+	}
 }
