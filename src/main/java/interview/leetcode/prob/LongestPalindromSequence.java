9ec6a2a8580d18b7bef180e8416cd7bf9886b761
Jojo Nandy <chiranjeeb.nandy@yahoo.com>
2020-08-12 22:34:59 -0700

    added solution

--- src/main/java/interview/leetcode/prob/LongestPalindromSequence.java
+++ src/main/java/interview/leetcode/prob/LongestPalindromSequence.java
@@ -14,6 +14,40 @@ package interview.leetcode.prob;
  * @author jojo Mar 20, 201711:48:47 PM
  */
 public class LongestPalindromSequence {
+	public int longestPalindromeSubseq_recursion(String s) {
+        Integer[][] memo = new Integer[s.length()][s.length()];
+        return recurse(s, 0, s.length() - 1, memo);
+    }
+    
+    private int recurse(String str, int i, int j, Integer[][] memo){
+        if(i > j){
+            return 0;
+        }
+        
+        if(i == j){
+            return 1;
+        }
+        
+        if(memo[i][j] != null){
+            return memo[i][j];
+        }
+        
+        if(str.charAt(i) == str.charAt(j)){
+            
+            memo[i][j] = 2 + recurse(str, i+1, j-1, memo);
+        }
+        else{
+            int val1 = recurse(str, i+1, j, memo);
+            int val2 = recurse(str, i, j-1, memo);
+            
+            memo[i][j] = Math.max(val1, val2);
+        }
+        
+        return memo[i][j];
+    }
+	
+        
+    // DP approach 1
     public int longestPalindromeSubseq(String s) {
         int len = s.length();
         int[][] dp = new int[len][len];
