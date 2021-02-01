a7ae9be2beec018ddfa68521ee7e7044ced43653
Jojo Nandy <chiranjeeb.nandy@yahoo.com>
2021-01-31 16:25:16 -0800

    Added solution

---
+++ src/main/java/interview/leetcode/prob/SplitAStringInABalancedString.java
@@ -0,0 +1,65 @@
+package interview.leetcode.prob;
+
+/**
+ * 
+Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+
+Given a balanced string s split it in the maximum amount of balanced strings.
+
+Return the maximum amount of splitted balanced strings.
+
+ 
+
+Example 1:
+
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+Example 2:
+
+Input: s = "RLLLLRRRLR"
+Output: 3
+Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+Example 3:
+
+Input: s = "LLLLRRRR"
+Output: 1
+Explanation: s can be split into "LLLLRRRR".
+Example 4:
+
+Input: s = "RLRRRLLRLL"
+Output: 2
+Explanation: s can be split into "RL", "RRRLLRLL", since each substring contains an equal number of 'L' and 'R'
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s[i] = 'L' or 'R'
+Accepted
+125,275
+Submissions
+148,981
+ * @author jojo
+ * Jan 31, 2021  3:58:47 PM
+ */
+public class SplitAStringInABalancedString {
+    public int balancedStringSplit(String s) {
+        int lCount = 0, result = 0;
+        
+        for(char ch : s.toCharArray()){
+            if(ch == 'L'){
+                lCount++;
+            }
+            else{
+                lCount--;
+            }
+            
+            if(lCount == 0){
+                result++;
+            }
+        }
+        
+        return result;
+    }
+}
