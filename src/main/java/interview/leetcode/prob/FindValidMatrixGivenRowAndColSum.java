25f7a03840c2d28b54703e71a136d616cf814176
Jojo Nandy <chiranjeeb.nandy@yahoo.com>
2021-01-31 19:06:38 -0800

    Added solution

---
+++ src/main/java/interview/leetcode/prob/FindValidMatrixGivenRowAndColSum.java
@@ -0,0 +1,100 @@
+package interview.leetcode.prob;
+
+/**
+ * You are given two arrays rowSum and colSum of non-negative integers where rowSum[i] is the sum of the elements in the ith row and colSum[j] is the sum of the elements of the jth column of a 2D matrix. In other words, you do not know the elements of the matrix, but you do know the sums of each row and column.
+
+Find any matrix of non-negative integers of size rowSum.length x colSum.length that satisfies the rowSum and colSum requirements.
+
+Return a 2D array representing any matrix that fulfills the requirements. It's guaranteed that at least one matrix that fulfills the requirements exists.
+
+ 
+
+Example 1:
+
+Input: rowSum = [3,8], colSum = [4,7]
+Output: [[3,0],
+         [1,7]]
+Explanation:
+0th row: 3 + 0 = 3 == rowSum[0]
+1st row: 1 + 7 = 8 == rowSum[1]
+0th column: 3 + 1 = 4 == colSum[0]
+1st column: 0 + 7 = 7 == colSum[1]
+The row and column sums match, and all matrix elements are non-negative.
+Another possible matrix is: [[1,2],
+                             [3,5]]
+Example 2:
+
+Input: rowSum = [5,7,10], colSum = [8,6,8]
+Output: [[0,5,0],
+         [6,1,0],
+         [2,0,8]]
+Example 3:
+
+Input: rowSum = [14,9], colSum = [6,9,8]
+Output: [[0,9,5],
+         [6,0,3]]
+Example 4:
+
+Input: rowSum = [1,0], colSum = [1]
+Output: [[1],
+         [0]]
+Example 5:
+
+Input: rowSum = [0], colSum = [0]
+Output: [[0]]
+ 
+
+Constraints:
+
+1 <= rowSum.length, colSum.length <= 500
+0 <= rowSum[i], colSum[i] <= 108
+sum(rows) == sum(columns)
+Accepted
+9,151
+Submissions
+11,818
+ * @author jojo
+ * Jan 31, 2021  7:06:12 PM
+ */
+public class FindValidMatrixGivenRowAndColSum {
+	// this runtime: O(n + m)
+	public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        int m = rowSum.length, n = colSum.length, i=0, j=0;
+        
+        int[][] arr = new int[m][n];
+        
+        while(i < m && j < n){
+            arr[i][j] = Math.min(rowSum[i], colSum[j]);
+            
+            rowSum[i] -= arr[i][j];
+            colSum[j] -= arr[i][j];
+            
+            if(rowSum[i] == 0){
+                i++;
+            }
+            else{ // colSum[j] == 0
+                j++;
+            }
+        }
+        
+        return arr;
+    }
+
+	// runtime : O(m*n)
+	public int[][] restoreMatrix_bruteforce(int[] rowSum, int[] colSum) {
+        int m = rowSum.length, n = colSum.length;
+        
+        int[][] arr = new int[m][n];
+        
+        for(int i=0; i<m; i++) {
+        	for(int j=0; j<n; j++) {
+        		arr[i][j] = Math.min(rowSum[i], colSum[j]);
+        		rowSum[i] -= arr[i][j];
+                colSum[j] -= arr[i][j];
+        	}
+        }
+        
+        return arr;
+    }
+	
+}
