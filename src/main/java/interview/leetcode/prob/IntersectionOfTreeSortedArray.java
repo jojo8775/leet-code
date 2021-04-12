1564101d21f8064643b561bc542c10a7ce1afc5c
Jojo Nandy <chiranjeeb.nandy@yahoo.com>
2021-04-11 23:09:25 -0700

    Added solution

---
+++ src/main/java/interview/leetcode/prob/IntersectionOfTreeSortedArray.java
@@ -0,0 +1,60 @@
+package interview.leetcode.prob;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+Given three integer arrays arr1, arr2 and arr3 sorted in strictly increasing order, return a sorted array of only the integers that appeared in all three arrays.
+
+ 
+
+Example 1:
+
+Input: arr1 = [1,2,3,4,5], arr2 = [1,2,5,7,9], arr3 = [1,3,4,5,8]
+Output: [1,5]
+Explanation: Only 1 and 5 appeared in the three arrays.
+Example 2:
+
+Input: arr1 = [197,418,523,876,1356], arr2 = [501,880,1593,1710,1870], arr3 = [521,682,1337,1395,1764]
+Output: []
+ 
+
+Constraints:
+
+1 <= arr1.length, arr2.length, arr3.length <= 1000
+1 <= arr1[i], arr2[i], arr3[i] <= 2000
+Accepted
+43,198
+Submissions
+54,381
+ * @author jojo
+ * Apr 11, 2021  11:09:10 PM
+ */
+public class IntersectionOfTreeSortedArray {
+    public List<Integer> arraysIntersection(int[] arr1, int[] arr2, int[] arr3) {
+        List<Integer> result = new ArrayList<>();
+        
+        int i=0, j=0, k=0;
+        
+        while(i < arr1.length && j < arr2.length && k < arr3.length){
+            if(arr1[i] == arr2[j] && arr2[j] == arr3[k]){
+                result.add(arr1[i]);
+                i++;
+                j++;
+                k++;
+            }
+            else if(arr1[i] < arr2[j]){
+                i++;
+            }
+            else if(arr2[j] < arr3[k]){
+                j++;
+            }
+            else{
+                k++;
+            }
+        }
+        
+        return result;
+    }
+}
