0d647837da4ab2327caf15da41c2637c7e22aad4
Jojo Nandy <chiranjeeb.nandy@yahoo.com>
2021-06-29 02:00:32 -0700

    added solution

---
+++ src/main/java/interview/leetcode/prob/MaximumProductDiffernceBetweenTwoNumbers.java
@@ -0,0 +1,62 @@
+package interview.leetcode.prob;
+
+/**
+ * The product difference between two pairs (a, b) and (c, d) is defined as (a * b) - (c * d).
+
+For example, the product difference between (5, 6) and (2, 7) is (5 * 6) - (2 * 7) = 16.
+Given an integer array nums, choose four distinct indices w, x, y, and z such that the product difference between pairs (nums[w], nums[x]) and (nums[y], nums[z]) is maximized.
+
+Return the maximum such product difference.
+
+ 
+
+Example 1:
+
+Input: nums = [5,6,2,7,4]
+Output: 34
+Explanation: We can choose indices 1 and 3 for the first pair (6, 7) and indices 2 and 4 for the second pair (2, 4).
+The product difference is (6 * 7) - (2 * 4) = 34.
+Example 2:
+
+Input: nums = [4,2,5,9,7,4,8]
+Output: 64
+Explanation: We can choose indices 3 and 6 for the first pair (9, 8) and indices 1 and 5 for the second pair (2, 4).
+The product difference is (9 * 8) - (2 * 4) = 64.
+ 
+
+Constraints:
+
+4 <= nums.length <= 104
+1 <= nums[i] <= 104
+Accepted
+7,970
+Submissions
+9,279
+ * @author jojo
+ * Jun 27, 2021  2:47:43 AM
+ */
+public class MaximumProductDiffernceBetweenTwoNumbers {
+    public int maxProductDifference(int[] nums) {
+        int min1 = Integer.MAX_VALUE, min2 = min1, max1 = Integer.MIN_VALUE, max2 = max1;
+        
+        for(int n : nums){
+            if(max1 <= n){
+                max2 = max1;
+                max1 = n;
+            }
+            else if(max2 <= n){
+                max2 = n;
+            }
+            
+            if(min1 >= n){
+                min2 = min1;
+                min1 = n;
+            }
+            else if(min2 >= n){
+                min2 = n;
+            }
+        }
+        
+        return (max1 * max2) - (min1 * min2);
+    }
+}
---
+++ src/main/java/interview/leetcode/prob/RemoveOneElementToMakeTheArrayStrictlyIncreasing.java
@@ -0,0 +1,81 @@
+package interview.leetcode.prob;
+
+/**
+ * Given a 0-indexed integer array nums, return true if it can be made strictly increasing after removing exactly one element, or false otherwise. If the array is already strictly increasing, return true.
+
+The array nums is strictly increasing if nums[i - 1] < nums[i] for each index (1 <= i < nums.length).
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,10,5,7]
+Output: true
+Explanation: By removing 10 at index 2 from nums, it becomes [1,2,5,7].
+[1,2,5,7] is strictly increasing, so return true.
+Example 2:
+
+Input: nums = [2,3,1,2]
+Output: false
+Explanation:
+[3,1,2] is the result of removing the element at index 0.
+[2,1,2] is the result of removing the element at index 1.
+[2,3,2] is the result of removing the element at index 2.
+[2,3,1] is the result of removing the element at index 3.
+No resulting array is strictly increasing, so return false.
+Example 3:
+
+Input: nums = [1,1,1]
+Output: false
+Explanation: The result of removing any element is [1,1].
+[1,1] is not strictly increasing, so return false.
+Example 4:
+
+Input: nums = [1,2,3]
+Output: true
+Explanation: [1,2,3] is already strictly increasing, so return true.
+ 
+
+Constraints:
+
+2 <= nums.length <= 1000
+1 <= nums[i] <= 1000
+Accepted
+7,683
+Submissions
+24,792
+ * @author jojo
+ * Jun 29, 2021  1:58:58 AM
+ */
+public class RemoveOneElementToMakeTheArrayStrictlyIncreasing {
+    public boolean canBeIncreasing(int[] nums) {
+        if(nums.length == 0){
+            return true;
+        }
+        
+        int leftCount = 0, rightCount = 0;
+        
+        int maxLeftToRight = nums[0];
+        
+        for(int i=1; i<nums.length; i++){
+            if(maxLeftToRight >= nums[i]){
+                leftCount++;
+            }
+            
+            maxLeftToRight = Math.max(maxLeftToRight, nums[i]);
+        }
+        
+        
+        int minRightToLeft = nums[nums.length - 1];
+        
+        for(int i=nums.length - 2; i>=0; i--){
+            if(minRightToLeft <= nums[i]){
+                rightCount++;
+            }
+            
+            minRightToLeft = Math.min(minRightToLeft, nums[i]);
+        }
+        
+        return leftCount <= 1 || rightCount <= 1;
+    }
+}
