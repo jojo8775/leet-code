8a8e6843ff3cd12f81248eb24dcbb890bc6b630a
Jojo Nandy <chiranjeeb.nandy@yahoo.com>
2021-11-03 21:42:46 -0700

    test

--- src/main/java/interview/leetcode/practice/Practice3.java
+++ src/main/java/interview/leetcode/practice/Practice3.java
@@ -3,6 +3,7 @@ package interview.leetcode.practice;
 public class Practice3 {
 	public static void main(String[] args) {
 		var ss = "hellow";
+		var ss1 = "new var";
 		System.out.println(ss);
 	}
 }
