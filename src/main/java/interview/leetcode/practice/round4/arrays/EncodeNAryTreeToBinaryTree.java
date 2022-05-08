c3b3a42aa1ba03e7a1d9fdd26b5737b221d02bce
Jojo Nandy <chiranjeeb.nandy@yahoo.com>
2022-05-08 16:57:41 -0700

    Added solution

---
+++ src/main/java/interview/leetcode/practice/round4/arrays/EncodeNAryTreeToBinaryTree.java
@@ -0,0 +1,133 @@
+package interview.leetcode.practice.round4.arrays;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Design an algorithm to encode an N-ary tree into a binary tree and decode the binary tree to get the original N-ary tree. An N-ary tree is a rooted tree in which each node has no more than N children. Similarly, a binary tree is a rooted tree in which each node has no more than 2 children. There is no restriction on how your encode/decode algorithm should work. You just need to ensure that an N-ary tree can be encoded to a binary tree and this binary tree can be decoded to the original N-nary tree structure.
+
+Nary-Tree input serialization is represented in their level order traversal, each group of children is separated by the null value (See following example).
+
+For example, you may encode the following 3-ary tree to a binary tree in this way:
+
+
+
+Input: root = [1,null,3,2,4,null,5,6]
+Note that the above is just an example which might or might not work. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
+
+ 
+
+Example 1:
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [1,null,3,2,4,null,5,6]
+Example 2:
+
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+0 <= Node.val <= 104
+The height of the n-ary tree is less than or equal to 1000
+Do not use class member/global/static variables to store states. Your encode and decode algorithms should be stateless.
+Accepted
+16,891
+Submissions
+21,662
+ * @author jojo
+ * May 8, 2022 4:57:17 PM
+ */
+public class EncodeNAryTreeToBinaryTree {
+	class Codec {
+	    // Encodes an n-ary tree to a binary tree.
+	    public TreeNode encode(Node root) {
+	        return createTreeNode(root);
+	    }
+	    
+	    private TreeNode createTreeNode(Node root){
+	        if(root == null){
+	            return null;
+	        }
+	        
+	        List<TreeNode> tNodes = new LinkedList<>();
+	        for(Node child : root.children){
+	            tNodes.add(createTreeNode(child));
+	        }
+	        
+	        TreeNode tNode = new TreeNode(root.val);
+	        if(tNodes.size() > 0){
+	            tNode.left = tNodes.get(0);
+	            
+	            TreeNode temp = tNode.left;
+	            for(int i=1; i<tNodes.size(); i++){
+	                temp.right = tNodes.get(i);
+	                temp = temp.right;
+	            }
+	        }
+	        
+	        return tNode;
+	    }
+		
+	    // Decodes your binary tree to an n-ary tree.
+	    public Node decode(TreeNode root) {
+	        return createNode(root);
+	    }
+	    
+	    private Node createNode(TreeNode root){
+	        if(root == null){
+	            return null;
+	        }
+	        
+	        List<Node> children = getChildren(root);
+	        
+	        Node node = new Node(root.val);
+	        node.children = children;
+	        
+	        return node;
+	    }
+	    
+	    private List<Node> getChildren(TreeNode node){
+	        List<Node> children = new ArrayList<>();
+	        
+	        if(node.left == null){
+	            return children;
+	        }
+	        
+	        node = node.left;
+	        children.add(createNode(node));
+	        
+	        while(node.right != null){
+	            children.add(createNode(node.right));
+	            node = node.right;
+	        }
+	        
+	        return children;
+	    }
+	}
+	
+	private static class TreeNode{
+		int val;
+		TreeNode left, right;
+		
+		public TreeNode(int val) {
+			this.val = val;
+		}
+	}
+	
+	private static class Node{
+		int val;
+		List<Node> children;
+		
+		public Node(int val) {
+			this.val = val;
+		}
+	}
+}
--- src/main/java/interview/leetcode/practice/round4/arrays/SubarraySumEqualsK.java
+++ src/main/java/interview/leetcode/practice/round4/arrays/SubarraySumEqualsK.java
@@ -3,20 +3,46 @@ package interview.leetcode.practice.round4.arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-1000 <= nums[i] <= 1000
+-107 <= k <= 107
+Accepted
+771,866
+Submissions
+1,744,777
+ * @author jojo
+ * May 7, 2022 11:47:17 PM
+ */
 public class SubarraySumEqualsK {
     public int subarraySum(int[] nums, int k) {
-        int count = 0, sum = 0;
-
-        Map<Integer, Integer> map = new HashMap<>();
+    	Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        int sum = 0;
         map.put(0, 1);
-
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-
-            count += map.getOrDefault((sum - k), 0);
+        for (int i = 0; i<nums.length; i++) {
+            sum+=nums[i];
+            if (map.containsKey(sum-k)) {
+                count+=map.get(sum-k);
+            }
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
-
         return count;
     }
 
