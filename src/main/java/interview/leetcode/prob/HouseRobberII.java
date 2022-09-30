f36d4065cc477da82c7e8802fafbb18c34f97744
Jojo Nandy <chiranjeeb.nandy@yahoo.com>
2022-09-30 00:15:59 -0700

    added solution

--- src/main/java/interview/leetcode/prob/HouseRobberII.java
+++ src/main/java/interview/leetcode/prob/HouseRobberII.java
@@ -21,6 +21,47 @@ package interview.leetcode.prob;
  *
  */
 public class HouseRobberII {
+	public int rob_easy_toUnderstand(int[] nums) {
+        int len = nums.length;
+        
+        if(len == 1){
+            return nums[0];
+        }   
+        
+        if(len == 2){
+            return Math.max(nums[0], nums[1]);
+        }
+        
+        // since this is a circular array, including first will excluse the last 
+        int includingFirst = findMax(nums, 0, len - 2);
+        
+        // if we include the last then first needs to be excluded
+        int excludingFirst = findMax(nums, 1, len - 1);
+        
+        return Math.max(includingFirst, excludingFirst);
+    }
+    
+    // this is same as House Robber 1 
+    private int findMax(int[] arr, int beg, int end){
+        int len = end - beg + 1;
+        int[] dp = new int[len];
+        
+        for(int i=0; i<len; i++){
+            if(i == 0){
+                dp[0] = arr[beg];
+            }
+            else if(i == 1){
+                dp[1] = Math.max(arr[beg], arr[beg + 1]);
+            }
+            else{
+                dp[i] = Math.max(dp[i-1], dp[i-2] + arr[beg + i]);
+            }
+        }
+        
+        return dp[len - 1];
+    }
+	
+	
 	public int rob(int[] nums) {
 		if (nums.length == 0) {
 			return 0;
