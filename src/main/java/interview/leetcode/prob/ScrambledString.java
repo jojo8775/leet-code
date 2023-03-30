f4495276d1c4bfc123fc6a5b19d2ff4ec2fafb2b
Jojo Nandy <chiranjeeb.nandy@yahoo.com>
2023-03-29 23:33:35 -0700

    added code

--- src/main/java/interview/leetcode/prob/ScrambledString.java
+++ src/main/java/interview/leetcode/prob/ScrambledString.java
@@ -1,5 +1,8 @@
 package interview.leetcode.prob;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given a string s1, we may represent it as a binary tree by partitioning it to two non-empty substrings recursively.
 
@@ -42,6 +45,57 @@ Given two strings s1 and s2 of the same length, determine if s2 is a scrambled s
  */
 public class ScrambledString {
 	public boolean isScramble(String s1, String s2) {
+		Map<String, Boolean> map = new HashMap<>();
+		return isScramble(s1, s2, map);
+	}
+
+	public boolean isScramble(String s1, String s2, Map<String, Boolean> map) {
+		if (s1.equals(s2)) {
+			return true;
+		}
+		
+		int s1Array[] = new int[26];
+		int s2Array[] = new int[26];
+		for (int i = 0; i < s1.length(); i++) {
+			s1Array[s1.charAt(i) - 'a']++;
+			s2Array[s2.charAt(i) - 'a']++;
+		}
+
+		String key = s1 + "-" + s2;
+
+		if (map.containsKey(key)) {
+			return map.get(key);
+		}
+
+		for (int i = 0; i < 26; i++)
+			if (s1Array[i] != s2Array[i]) {
+				map.put(key, false);
+				return map.get(key);
+			}
+
+		boolean flag = false;
+
+		for (int i = 1; i < s1.length(); i++) {
+			if (isScramble(s1.substring(0, i), s2.substring(0, i), map)
+					&& isScramble(s1.substring(i), s2.substring(i), map)) {
+				flag = true;
+				break;
+			}
+
+			if (isScramble(s1.substring(0, i), s2.substring(s1.length() - i), map)
+					&& isScramble(s1.substring(i), s2.substring(0, s1.length() - i), map)) {
+				flag = true;
+				break;
+			}
+
+		}
+
+		map.put(key, flag);
+		return map.get(key);
+	}
+	
+	
+	public boolean isScramble_old(String s1, String s2) {
         // check if both string are equal
         if(s1.equals(s2)){
             return true;
@@ -69,13 +123,13 @@ public class ScrambledString {
         
         //recursive split
         for(int i=1; i<s1.length(); i++){
-            if(isScramble(s1.substring(0,i), s2.substring(0,i)) 
-                && isScramble(s1.substring(i), s2.substring(i))){
+            if(isScramble_old(s1.substring(0,i), s2.substring(0,i)) 
+                && isScramble_old(s1.substring(i), s2.substring(i))){
                 return true;
             }
             
-            if(isScramble(s1.substring(0,i), s2.substring(s2.length() - i)) 
-                && isScramble(s1.substring(i), s2.substring(0, s2.length() - i))){
+            if(isScramble_old(s1.substring(0,i), s2.substring(s2.length() - i)) 
+                && isScramble_old(s1.substring(i), s2.substring(0, s2.length() - i))){
                 return true;
             }
         }
