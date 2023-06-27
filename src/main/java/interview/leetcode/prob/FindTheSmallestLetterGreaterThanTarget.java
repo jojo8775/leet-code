368320added667d951a155c4fd034df11697578c
jojo <chiranjeeb.nandy@yahoo.com>
2023-06-27 00:02:38 -0700

    added sol

--- src/main/java/interview/leetcode/prob/FindTheSmallestLetterGreaterThanTarget.java
+++ src/main/java/interview/leetcode/prob/FindTheSmallestLetterGreaterThanTarget.java
@@ -1,62 +1,58 @@
 package interview.leetcode.prob;
 
 /**
- * You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
-
-Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
-
- 
-
-Example 1:
-
-Input: letters = ["c","f","j"], target = "a"
-Output: "c"
-Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
-Example 2:
-
-Input: letters = ["c","f","j"], target = "c"
-Output: "f"
-Explanation: The smallest character that is lexicographically greater than 'c' in letters is 'f'.
-Example 3:
-
-Input: letters = ["x","x","y","y"], target = "z"
-Output: "x"
-Explanation: There are no characters in letters that is lexicographically greater than 'z' so we return letters[0].
- 
-
-Constraints:
-
-2 <= letters.length <= 104
-letters[i] is a lowercase English letter.
-letters is sorted in non-decreasing order.
-letters contains at least two different characters.
-target is a lowercase English letter.
-Accepted
-327,780
-Submissions
-684,153
- * @author jojo
- * Jun. 8, 2023 11:43:23 p.m.
+ * You are given an array of characters letters that is sorted in non-decreasing
+ * order, and a character target. There are at least two different characters in
+ * letters.
+ * 
+ * Return the smallest character in letters that is lexicographically greater
+ * than target. If such a character does not exist, return the first character
+ * in letters.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: letters = ["c","f","j"], target = "a" Output: "c" Explanation: The
+ * smallest character that is lexicographically greater than 'a' in letters is
+ * 'c'. Example 2:
+ * 
+ * Input: letters = ["c","f","j"], target = "c" Output: "f" Explanation: The
+ * smallest character that is lexicographically greater than 'c' in letters is
+ * 'f'. Example 3:
+ * 
+ * Input: letters = ["x","x","y","y"], target = "z" Output: "x" Explanation:
+ * There are no characters in letters that is lexicographically greater than 'z'
+ * so we return letters[0].
+ * 
+ * 
+ * Constraints:
+ * 
+ * 2 <= letters.length <= 104 letters[i] is a lowercase English letter. letters
+ * is sorted in non-decreasing order. letters contains at least two different
+ * characters. target is a lowercase English letter. Accepted 327,780
+ * Submissions 684,153
+ * 
+ * @author jojo Jun. 8, 2023 11:43:23 p.m.
  */
 public class FindTheSmallestLetterGreaterThanTarget {
 	public char nextGreatestLetter(char[] letters, char target) {
-        int beg = 0, end = letters.length;
-        
-        while(beg < end){
-            int mid = beg + (end - beg)/2;
-            
-            if(letters[mid] <= target){
-                beg = mid + 1;
-            }
-            else{
-                end = mid;
-            }
-        }
-        
-        if(beg == letters.length){
-            return letters[0];
-        }
-        
-        return letters[beg];
-    }
+		int beg = 0, end = letters.length;
+
+		while (beg < end) {
+			int mid = beg + (end - beg) / 2;
+
+			if (letters[mid] <= target) {
+				beg = mid + 1;
+			} else {
+				end = mid;
+			}
+		}
+
+		if (beg == letters.length) {
+			return letters[0];
+		}
+
+		return letters[beg];
+	}
 }
--- src/main/java/interview/leetcode/prob/MinimumFlipToMakeAorBEqualToC.java
+++ src/main/java/interview/leetcode/prob/MinimumFlipToMakeAorBEqualToC.java
@@ -1,61 +1,51 @@
 package interview.leetcode.prob;
 
 /**
- * Given 3 positives numbers a, b and c. Return the minimum flips required in some bits of a and b to make ( a OR b == c ). (bitwise OR operation).
-Flip operation consists of change any single bit 1 to 0 or change the bit 0 to 1 in their binary representation.
-
- 
-
-Example 1:
-
-
-
-Input: a = 2, b = 6, c = 5
-Output: 3
-Explanation: After flips a = 1 , b = 4 , c = 5 such that (a OR b == c)
-Example 2:
-
-Input: a = 4, b = 2, c = 7
-Output: 1
-Example 3:
-
-Input: a = 1, b = 2, c = 3
-Output: 0
- 
-
-Constraints:
-
-1 <= a <= 10^9
-1 <= b <= 10^9
-1 <= c <= 10^9
-Accepted
-86,697
-Submissions
-121,040
- * @author jojo
- * Jun. 8, 2023 12:29:17 a.m.
+ * Given 3 positives numbers a, b and c. Return the minimum flips required in
+ * some bits of a and b to make ( a OR b == c ). (bitwise OR operation). Flip
+ * operation consists of change any single bit 1 to 0 or change the bit 0 to 1
+ * in their binary representation.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * Input: a = 2, b = 6, c = 5 Output: 3 Explanation: After flips a = 1 , b = 4 ,
+ * c = 5 such that (a OR b == c) Example 2:
+ * 
+ * Input: a = 4, b = 2, c = 7 Output: 1 Example 3:
+ * 
+ * Input: a = 1, b = 2, c = 3 Output: 0
+ * 
+ * 
+ * Constraints:
+ * 
+ * 1 <= a <= 10^9 1 <= b <= 10^9 1 <= c <= 10^9 Accepted 86,697 Submissions
+ * 121,040
+ * 
+ * @author jojo Jun. 8, 2023 12:29:17 a.m.
  */
 public class MinimumFlipToMakeAorBEqualToC {
-    public int minFlips(int a, int b, int c) {
-        int count = 0;
-        
-        while(a != 0 || b != 0 || c != 0){
-            if((c & 1) == 1){
-                if(((a & 1) == 0) && ((b & 1) == 0)){
-                    count ++;
-                }
-            }
-            else{
-                count += (a & 1);
-                count += (b & 1);
-            }
-            
-            
-            a >>= 1;
-            b >>= 1;
-            c >>= 1;
-        }
-        
-        return count;
-    }
+	public int minFlips(int a, int b, int c) {
+		int count = 0;
+
+		while (a != 0 || b != 0 || c != 0) {
+			if ((c & 1) == 1) {
+				if (((a & 1) == 0) && ((b & 1) == 0)) {
+					count++;
+				}
+			} else {
+				count += (a & 1);
+				count += (b & 1);
+			}
+
+			a >>= 1;
+			b >>= 1;
+			c >>= 1;
+		}
+
+		return count;
+	}
 }
